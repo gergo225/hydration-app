55cc113b2ba15554796ba77b4ca59b223c1392b8
gergo225 <fazekasgergo225@gmail.com>
2022-01-25 23:03:18 +0200

    Add custom toolbar with centered title

--- app/src/main/java/com/gergo225/hydrationapp/MainActivity.kt
+++ app/src/main/java/com/gergo225/hydrationapp/MainActivity.kt
@@ -19,9 +19,15 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSupportActionBar(binding.customToolbar)
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            binding.toolbarTitleText.text = destination.label
+        }
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
@@ -32,4 +38,5 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
 }
